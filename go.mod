5b5772e6557cdf605972d680daf4d030744df571
Bankole Ojo <ojobankole@gmail.com>
2024-10-14 10:00:01 +0100

    Init and GetNewForm functions

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/saenuma/forms824
+
+go 1.23.2
+
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/saenuma/flaarum v1.12.5
+)
+
+require (
+	github.com/adam-hanna/arrayOperations v1.0.1 // indirect
+	github.com/saenuma/zazabul v1.1.4 // indirect
+)
---
+++ lib.go
@@ -0,0 +1,129 @@
+package forms824
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"slices"
+	"strings"
+
+	"github.com/pkg/errors"
+	"github.com/saenuma/flaarum"
+)
+
+func doesPathExists(p string) bool {
+	if _, err := os.Stat(p); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func getFlaarumStmt(p string) string {
+	rawJSON, err := os.ReadFile(p)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	formObjects := make([]map[string]string, 0)
+	json.Unmarshal(rawJSON, &formObjects)
+
+	tableName := strings.ReplaceAll(filepath.Base(p), ".f8p", "")
+	stmt := "table: " + tableName + "\n"
+	stmt += "fields:\n"
+	for _, obj := range formObjects {
+		var flaarumField string
+		if slices.Index([]string{"email", "select", "string", "date", "datetime"}, obj["fieldtype"]) != -1 {
+			flaarumField = "string"
+		} else if obj["fieldtype"] == "number" {
+			flaarumField = "int"
+		} else if obj["fieldtype"] == "text" {
+			flaarumField = "text"
+		}
+		attribs := strings.Split(obj["attributes"], ";")
+		stmt += fmt.Sprintf("%s %s %s\n", obj["name"], flaarumField, strings.Join(attribs, " "))
+	}
+	stmt += "::"
+
+	return stmt
+}
+
+type F8Object struct {
+	FormsObjectPath string
+	FlaarumClient   flaarum.Client
+}
+
+func Init(formObjectsPath string, cl flaarum.Client) (F8Object, error) {
+	if !doesPathExists(formObjectsPath) {
+		return F8Object{}, errors.New(fmt.Sprintf("formsObjectPath %s does not exists.", formObjectsPath))
+	}
+
+	if err := cl.Ping(); err != nil {
+		return F8Object{}, errors.Wrap(err, "flaarum error")
+	}
+
+	dirFIs, err := os.ReadDir(formObjectsPath)
+	if err != nil {
+		return F8Object{}, errors.Wrap(err, "os error")
+	}
+
+	for _, dirFI := range dirFIs {
+		if strings.HasSuffix(dirFI.Name(), ".f8p") {
+			stmt := getFlaarumStmt(filepath.Join(formObjectsPath, dirFI.Name()))
+			err = cl.CreateOrUpdateTable(stmt)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+
+	return F8Object{formObjectsPath, cl}, nil
+}
+
+func (f8o *F8Object) GetNewForm(formName string) (string, error) {
+	if !strings.HasSuffix(formName, ".f8p") {
+		formName += ".f8p"
+	}
+	formPath := filepath.Join(f8o.FormsObjectPath, formName)
+
+	rawJSON, err := os.ReadFile(formPath)
+	if err != nil {
+		return "", errors.Wrap(err, "json error")
+	}
+
+	formObjects := make([]map[string]string, 0)
+	json.Unmarshal(rawJSON, &formObjects)
+
+	var html string
+	for _, obj := range formObjects {
+		html += "<div>"
+		html += fmt.Sprintf("<div><label for='id_%s'>%s</label></div>", obj["name"], obj["label"])
+		if slices.Index([]string{"number", "string", "email", "date", "datetime"}, obj["fieldtype"]) != -1 {
+			html += fmt.Sprintf("<input type='%s' name='%s' id='id_%s' ", obj["fieldtype"],
+				obj["name"], obj["name"])
+			if slices.Index(strings.Split(obj["attributes"], ";"), "required") != -1 {
+				html += " required"
+			}
+			html += "/>"
+		} else if obj["fieldtype"] == "select" {
+			html += fmt.Sprintf("<select id='id_%s' name='%s'", obj["name"], obj["name"])
+			if slices.Index(strings.Split(obj["attributes"], ";"), "required") != -1 {
+				html += " required"
+			}
+			html += ">"
+			for _, opt := range strings.Split(obj["select_options"], "\n") {
+				html += "<option>" + opt + "</option>"
+			}
+			html += "</select>"
+		} else if obj["fieldtype"] == "text" {
+			html += fmt.Sprintf("<textarea id='id_%' name='%s'")
+			if slices.Index(strings.Split(obj["attributes"], ";"), "required") != -1 {
+				html += " required"
+			}
+			html += "><textarea/>"
+		}
+		html += "</div>"
+	}
+
+	return html, nil
+}
